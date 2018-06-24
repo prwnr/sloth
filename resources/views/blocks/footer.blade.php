ea58c71ac8a2f9f4eb18a393c4fe09b30bb08cdd
Rafał Purwin <purwinr@gmail.com>
2018-06-24 16:36:59 +0200

    cutting app layout into separate blocks

---
+++ resources/views/blocks/footer.blade.php
@@ -0,0 +1,4 @@
+<!-- Main Footer -->
+<footer class="main-footer position-sticky">
+    <strong>Copyright &copy; 2018 <a href="https://semicket.com">semicket.com</a>.</strong> All rights reserved.
+</footer>
