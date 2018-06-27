457ccdcabae5ed95d774e7f6c22c5d0cc3433efb
Rafał Purwin <purwinr@gmail.com>
2018-06-27 22:30:06 +0200

    fixing sweet alert font and select2 styles from template

--- resources/assets/js/components/cruds/Members/Edit.vue
+++ resources/assets/js/components/cruds/Members/Edit.vue
@@ -85,7 +85,7 @@
 
             <hr>
             <div class="row">
-                <div class="col-lg-6">
+                <div class="col-lg-6 mb-3">
                     <form @submit.prevent="changePassword" @keydown="formPassword.errors.clear($event.target.name)">
                         <div class="card mt-3">
                             <card-header>Member password</card-header>
