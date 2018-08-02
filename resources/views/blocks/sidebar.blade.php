8e8fccf929a4ce3343e55bfd0585594dcccc3295
Rafał Purwin <purwinr@gmail.com>
2018-08-02 19:36:55 +0200

    logo icon change

--- resources/views/blocks/sidebar.blade.php
+++ resources/views/blocks/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <router-link :to="{ name: 'dashboard' }" class="brand-link">
-        <img src="{{ asset('images/static/semicket_white.png') }}" alt="" class="brand-image img-circle elevation-3"
+        <img src="{{ asset('images/static/sloth.png') }}" alt="" class="brand-image img-circle elevation-3"
              style="opacity: .8">
         <span class="brand-text font-weight-normal">Sloth</span>
     </router-link>
