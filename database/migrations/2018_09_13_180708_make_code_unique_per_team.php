846be3c6b719aab51349df17b4ada6c14c0223ef
Rafał Purwin <purwinr@gmail.com>
2018-09-22 10:59:01 +0200

    class name changed for migration

--- database/migrations/2018_09_13_180708_make_code_unique_per_team.php
+++ database/migrations/2018_09_13_180708_make_code_unique_per_team.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class MakeCodeAndEmailUniquePerTeam extends Migration
+class MakeCodeUniquePerTeam extends Migration
 {
     /**
      * Run the migrations.
