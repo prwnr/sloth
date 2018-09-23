04f6414b13b6ed18ccdebdbd5bcd3b0aa3e0ff90
Rafał Purwin <purwinr@gmail.com>
2018-09-23 17:23:18 +0200

    chartjs for vue installed, added Line and Bar charts, added BarChart to dashboard with test data

--- resources/js/views/Dashboard.vue
+++ resources/js/views/Dashboard.vue
@@ -1,28 +1,49 @@
 <template>
     <div class="content">
         <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-10">
-                        <h1>Dashboard</h1>
-                    </div>
-                </div>
-            </div>
         </section>
         <section class="content">
-            <div class="row col-lg-12">
-                Content
+            <div class="row ">
+                <div class="col-lg-12">
+                    <div class="col-lg-6 mb-3">
+                        <div class="card">
+                            <card-header :minimizable="false">Total hours worked per month</card-header>
+                            <div class="card-body">
+                                <bar-chart :data="chartData"></bar-chart>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     </div>
 </template>
 
 <script>
+    import BarChart from '../components/Charts/BarChart';
+
     export default {
+        components: {
+            BarChart
+        },
         data() {
             return {
+                chartData: {
+                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'Septemer', 'October', 'November', 'December'],
+                    datasets: [
+                        {
+                            label: 'total hours worked',
+                            backgroundColor: '#f87979',
+                            data: [0, 0, 0, 100, 20, 100, 170, 30, 0, 0, 0, 12]
+                        }
+                    ]
+                }
 
             }
+        },
+
+        created() {
+            axios.get('api/');
         }
     }
 </script>
