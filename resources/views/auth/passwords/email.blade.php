12522aea785c8fe56e5819b82d9705cf1653aa4e
Rafał Purwin <purwinr@gmail.com>
2018-10-29 18:05:03 +0100

    auth views layout changed

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,36 +1,31 @@
 @extends('layouts.auth')
 
 @section('content')
-    <div class="card card-login mx-auto">
-        <div class="card-header text-center"><h4>{{ __('Reset Password') }}</h4></div>
-        <div class="card-body">
-            @if (session('status'))
-                <div class="alert alert-success">
-                    {{ session('status') }}
-                </div>
-            @endif
-
-            <div class="text-center mt-4 mb-5">
+    <div>
+        <h2 class="text-center">Reset password</h2>
+        <form method="POST" action="{{ route('password.email') }}">
+            <div class="text-center mt-4 mb-3">
                 <h4>{{ __('Forgot your password?') }}</h4>
                 <p>{{ __('Enter your email address and we will send you instructions on how to reset your password.') }}</p>
             </div>
 
-            <form method="POST" action="{{ route('password.email') }}">
-                @csrf
+            @csrf
+            <div class="form-group">
+                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                       name="email" value="{{ old('email') }}" required placeholder="{{ __('E-Mail Address') }}">
 
-                <div class="form-group">
-                    <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
-                           name="email" value="{{ old('email') }}" required placeholder="{{ __('E-Mail Address') }}">
-
-                    @if ($errors->has('email'))
-                        <span class="invalid-feedback"><strong>{{ $errors->first('email') }}</strong></span>
-                    @endif
-                </div>
-                <button type="submit" class="btn btn-primary btn-block">{{ __('Send Password Reset Link') }}</button>
-            </form>
-            <div class="text-center">
-                <a class="d-block mt-3" href="{{ route('register') }}">{{ __('Register an Account') }}</a>
-                <a class="d-block" href="{{ route('login') }}">{{ __('Login Page') }}</a>
+                @if ($errors->has('email'))
+                    <span class="invalid-feedback"><strong>{{ $errors->first('email') }}</strong></span>
+                @endif
+            </div>
+            <button type="submit" class="btn btn-success btn-block">{{ __('Send Password Reset Link') }}</button>
+        </form>
+        <div class="copy-text">
+            <div class="d-block">
+                <a class="mt-3" href="{{ route('register') }}">{{ __('Dont have account yet?') }}</a>
+            </div>
+            <div class="d-block">
+                <a class="d-block" href="{{ route('login') }}">{{ __('Already have account? Log in!') }}</a>
             </div>
         </div>
     </div>
