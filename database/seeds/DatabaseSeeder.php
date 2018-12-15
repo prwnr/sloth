7eb9ba19ada10b060efddb2c0fedc363bb971e08
Rafał Purwin <purwinr@gmail.com>
2018-12-15 12:35:20 +0100

    fixed database seeder namespace use

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 use App\Models\Currency;
 use App\Models\Permission;
 use App\Models\Project;
+use App\Models\Project\Task;
 use App\Models\Role;
-use App\Models\Task;
 use App\Models\Team\Member;
 use App\Models\User;
 use Illuminate\Database\Seeder;
