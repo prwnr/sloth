c0ea059c19912de540dbb3392e853d651b03854c
Rafał Purwin <purwinr@gmail.com>
2019-01-04 19:41:13 +0100

    preparing backend for authorization with vue

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,3 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Auth::routes();
-
-Route::group(['middleware' => ['guest']], function () {
-    //Guest routes
-});
-
-Route::group(['middleware' => ['auth']], function () {
-    Route::post('changePassword', 'Auth\ChangePasswordController@changePassword')->name('password.change');
-    Route::get('changePassword', 'Auth\ChangePasswordController@index')->name('password.change.index');
-});
-
-Route::group(['middleware' => ['auth', 'firstLogin']], function () {
-    Route::get('/{any}', 'HomeController@index')->where('any', '.*');
-});
+Route::get('/{any}', 'DefaultController@index')->where('any', '.*');
