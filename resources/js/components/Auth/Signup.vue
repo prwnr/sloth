f953601be9dd38a9f7dd8d8b2c7e427c5e18ed76
Rafał Purwin <purwinr@gmail.com>
2019-01-15 19:47:19 +0100

    fixed registration form issues

--- resources/js/components/Auth/Signup.vue
+++ resources/js/components/Auth/Signup.vue
@@ -5,11 +5,11 @@
             <div class="form-group">
                 <div class="form-row">
                     <div class="col-md-6">
-                        <input id="firstname" type="text" class="form-control" :class="{ 'is-invalid': form.errors.has('email')}"
+                        <input id="firstname" type="text" class="form-control" :class="{ 'is-invalid': form.errors.has('firstname')}"
                                name="firstname" v-model="form.firstname" autofocus placeholder="First name">
                     </div>
                     <div class="col-md-6">
-                        <input id="lastname" type="text" class="form-control" :class="{ 'is-invalid': form.errors.has('email')}"
+                        <input id="lastname" type="text" class="form-control" :class="{ 'is-invalid': form.errors.has('lastname')}"
                                name="lastname" v-model="form.lastname" autofocus placeholder="Last name">
                     </div>
 
@@ -19,7 +19,7 @@
             </div>
 
             <div class="form-group">
-                <input id="team_name" type="text" class="form-control" :class="{ 'is-invalid': form.errors.has('email')}"
+                <input id="team_name" type="text" class="form-control" :class="{ 'is-invalid': form.errors.has('team_name')}"
                        name="team_name" v-model="form.team_name" placeholder="Team name">
                 <form-error :text="form.errors.get('team_name')" :show="form.errors.has('team_name')"></form-error>
             </div>
@@ -32,20 +32,19 @@
             <div class="form-group">
                 <div class="form-row">
                     <div class="col-md-6">
-                        <input id="password" type="password" class="form-control" :class="{ 'is-invalid': form.errors.has('email')}"
+                        <input id="password" type="password" class="form-control" :class="{ 'is-invalid': form.errors.has('password')}"
                                name="password" v-model="form.password" placeholder="Password">
-                        <form-error :text="form.errors.get('password')" :show="form.errors.has('password')"></form-error>
                     </div>
                     <div class="col-md-6">
-                        <input id="password-confirm" type="password" v-model="form.password_confirmation" :class="{ 'is-invalid': form.errors.has('email')}"
+                        <input id="password-confirm" type="password" v-model="form.password_confirmation" :class="{ 'is-invalid': form.errors.has('password_confirmation')}"
                                class="form-control" name="password_confirmation" placeholder="Confirm password">
-                        <form-error :text="form.errors.get('password_confirmation')" :show="form.errors.has('password_confirmation')"></form-error>
                     </div>
+                    <form-error :text="form.errors.get('password')" :show="form.errors.has('password')"></form-error>
                 </div>
             </div>
             <button class="btn btn-success btn-block float-right">Register</button>
         </form>
-        <div class="copy-text">
+        <div class="copy-text" :class="withErrors">
             <div class="d-block">
                 <router-link class="mt-3" :to="{ name: 'login' }" tag="a">Already have account? Log in!</router-link>
             </div>
@@ -75,10 +74,15 @@
             }
         },
 
+        computed: {
+            withErrors() {
+                return this.form.errors.any() ? 'with-errors' : '';
+            }
+        },
+
         methods: {
             ...mapActions(['logIn']),
             async submit() {
-                this.form.password_confirmation = this.form.password
                 await this.form.post('auth/signup').then(async (response) => {
                     await this.logIn(this.form.data()).then(async (token) => {
                         this.$cookie.set('auth-token', token.access_token, { expires:  token.expires_at})
@@ -104,5 +108,10 @@
 </script>
 
 <style scoped>
-
+    .with-errors {
+        position: inherit !important;
+        width: 100%;
+        margin-top: 65px;
+        margin-bottom: -30px;
+    }
 </style>
